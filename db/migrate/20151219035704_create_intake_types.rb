a20c7b1156317d341e95183cbb5ea9b9f9f1c936
Chip Young <fetchsoft@gmai.com>
2015-12-21 10:52:20 -0600

    US11-TA27: Create Intake Types Migration and Model

--- db/migrate/20151219035704_create_intake_types.rb
+++ db/migrate/20151219035704_create_intake_types.rb
@@ -10,9 +10,14 @@ class CreateIntakeTypes < ActiveRecord::Migration
     end
 
     # Add default intake types
-    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Stray', 0, 'Animal found or turned in as stray', now(), now())")
-    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Owner Surrender', 0, 'Animal surrendered by former owner', now(), now())")
-    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Public Assist', 0, 'Animal turned in by legal authority', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Stray', 0, 'Stray animal brought to animal care facility', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Owner Surrender', 0, 'Animal surrendered by former owner to animal care facility', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Transfer', 0, 'Animal transferred from another animal care facility', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Public Assist', 0, 'Animal turned in by legal authority to animal care facility', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Wildlife', 0, 'Wildlife Intake request', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Euthanasia Request', 0, 'Former owner requests humane euthanasia', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Other', 0, 'Not a stray, owner surrender, transfer, public assist, or euthanasia request', now(), now())")
+    conn.execute("INSERT INTO intake_types (name, group_id, description, created_at, updated_at) VALUES ('Unknown', 0, 'Intake type not yet determined', now(), now())")
   end
 
   def down
