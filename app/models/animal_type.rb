d36b1f404a5ac2772bbb9d25e04b0559445d336a
Chip Young <fetchsoft@gmai.com>
2015-12-21 15:04:17 -0600

    DE4 - default scope has wrong table name

--- app/models/animal_type.rb
+++ app/models/animal_type.rb
@@ -1,6 +1,6 @@
 class AnimalType < MultiTenantBase
   # always filter query by group_id
-  default_scope { self.current ? where('animals.group_id = ?', self.current) : nil }
+  default_scope { self.current ? where('animal_types.group_id = ?', self.current) : nil }
 
   validates :name, :group_id, presence: true
   validates :name, uniqueness: {scope: :group_id}
