d2a8d89e5b76cde507c36b61455330ef8a4c888e
Chip Young <fetchsoft@gmai.com>
2016-03-16 12:25:00 -0500

    Uncommented test (sheesh!)

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -24,137 +24,137 @@ RSpec.describe AnimalsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Animal. As you add validations to Animal, be sure to
   # adjust the attributes here as well.
-#  let(:valid_attributes) {
-#    skip("Add a hash of attributes valid for your model")
-#  }
-#
-#  let(:invalid_attributes) {
-#    skip("Add a hash of attributes invalid for your model")
-#  }
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # AnimalsController. Be sure to keep this updated too.
-#  let(:valid_session) { {} }
-#
-#  describe "GET #index" do
-#    it "assigns all animals as @animals" do
-#      animal = Animal.create! valid_attributes
-#      get :index, {}, valid_session
-#      expect(assigns(:animals)).to eq([animal])
-#    end
-#  end
-#
-#  describe "GET #show" do
-#    it "assigns the requested animal as @animal" do
-#      animal = Animal.create! valid_attributes
-#      get :show, {:id => animal.to_param}, valid_session
-#      expect(assigns(:animal)).to eq(animal)
-#    end
-#  end
-#
-#  describe "GET #new" do
-#    it "assigns a new animal as @animal" do
-#      get :new, {}, valid_session
-#      expect(assigns(:animal)).to be_a_new(Animal)
-#    end
-#  end
-#
-#  describe "GET #edit" do
-#    it "assigns the requested animal as @animal" do
-#      animal = Animal.create! valid_attributes
-#      get :edit, {:id => animal.to_param}, valid_session
-#      expect(assigns(:animal)).to eq(animal)
-#    end
-#  end
-#
-#  describe "POST #create" do
-#    context "with valid params" do
-#      it "creates a new Animal" do
-#        expect {
-#          post :create, {:animal => valid_attributes}, valid_session
-#        }.to change(Animal, :count).by(1)
-#      end
-#
-#      it "assigns a newly created animal as @animal" do
-#        post :create, {:animal => valid_attributes}, valid_session
-#        expect(assigns(:animal)).to be_a(Animal)
-#        expect(assigns(:animal)).to be_persisted
-#      end
-#
-#      it "redirects to the created animal" do
-#        post :create, {:animal => valid_attributes}, valid_session
-#        expect(response).to redirect_to(Animal.last)
-#      end
-#    end
-#
-#    context "with invalid params" do
-#      it "assigns a newly created but unsaved animal as @animal" do
-#        post :create, {:animal => invalid_attributes}, valid_session
-#        expect(assigns(:animal)).to be_a_new(Animal)
-#      end
-#
-#      it "re-renders the 'new' template" do
-#        post :create, {:animal => invalid_attributes}, valid_session
-#        expect(response).to render_template("new")
-#      end
-#    end
-#  end
-#
-#  describe "PUT #update" do
-#    context "with valid params" do
-#      let(:new_attributes) {
-#        skip("Add a hash of attributes valid for your model")
-#      }
-#
-#      it "updates the requested animal" do
-#        animal = Animal.create! valid_attributes
-#        put :update, {:id => animal.to_param, :animal => new_attributes}, valid_session
-#        animal.reload
-#        skip("Add assertions for updated state")
-#      end
-#
-#      it "assigns the requested animal as @animal" do
-#        animal = Animal.create! valid_attributes
-#        put :update, {:id => animal.to_param, :animal => valid_attributes}, valid_session
-#        expect(assigns(:animal)).to eq(animal)
-#      end
-#
-#      it "redirects to the animal" do
-#        animal = Animal.create! valid_attributes
-#        put :update, {:id => animal.to_param, :animal => valid_attributes}, valid_session
-#        expect(response).to redirect_to(animal)
-#      end
-#    end
-#
-#    context "with invalid params" do
-#      it "assigns the animal as @animal" do
-#        animal = Animal.create! valid_attributes
-#        put :update, {:id => animal.to_param, :animal => invalid_attributes}, valid_session
-#        expect(assigns(:animal)).to eq(animal)
-#      end
-#
-#      it "re-renders the 'edit' template" do
-#        animal = Animal.create! valid_attributes
-#        put :update, {:id => animal.to_param, :animal => invalid_attributes}, valid_session
-#        expect(response).to render_template("edit")
-#      end
-#    end
-#  end
-#
-#  describe "DELETE #destroy" do
-#    it "destroys the requested animal" do
-#      animal = Animal.create! valid_attributes
-#      expect {
-#        delete :destroy, {:id => animal.to_param}, valid_session
-#      }.to change(Animal, :count).by(-1)
-#    end
-#
-#    it "redirects to the animals list" do
-#      animal = Animal.create! valid_attributes
-#      delete :destroy, {:id => animal.to_param}, valid_session
-#      expect(response).to redirect_to(animals_url)
-#    end
-#  end
-#
+  let(:valid_session) { {} }
+
+  describe "GET #index" do
+    it "assigns all animals as @animals" do
+      animal = Animal.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:animals)).to eq([animal])
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested animal as @animal" do
+      animal = Animal.create! valid_attributes
+      get :show, {:id => animal.to_param}, valid_session
+      expect(assigns(:animal)).to eq(animal)
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new animal as @animal" do
+      get :new, {}, valid_session
+      expect(assigns(:animal)).to be_a_new(Animal)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested animal as @animal" do
+      animal = Animal.create! valid_attributes
+      get :edit, {:id => animal.to_param}, valid_session
+      expect(assigns(:animal)).to eq(animal)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Animal" do
+        expect {
+          post :create, {:animal => valid_attributes}, valid_session
+        }.to change(Animal, :count).by(1)
+      end
+
+      it "assigns a newly created animal as @animal" do
+        post :create, {:animal => valid_attributes}, valid_session
+        expect(assigns(:animal)).to be_a(Animal)
+        expect(assigns(:animal)).to be_persisted
+      end
+
+      it "redirects to the created animal" do
+        post :create, {:animal => valid_attributes}, valid_session
+        expect(response).to redirect_to(Animal.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved animal as @animal" do
+        post :create, {:animal => invalid_attributes}, valid_session
+        expect(assigns(:animal)).to be_a_new(Animal)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:animal => invalid_attributes}, valid_session
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      it "updates the requested animal" do
+        animal = Animal.create! valid_attributes
+        put :update, {:id => animal.to_param, :animal => new_attributes}, valid_session
+        animal.reload
+        skip("Add assertions for updated state")
+      end
+
+      it "assigns the requested animal as @animal" do
+        animal = Animal.create! valid_attributes
+        put :update, {:id => animal.to_param, :animal => valid_attributes}, valid_session
+        expect(assigns(:animal)).to eq(animal)
+      end
+
+      it "redirects to the animal" do
+        animal = Animal.create! valid_attributes
+        put :update, {:id => animal.to_param, :animal => valid_attributes}, valid_session
+        expect(response).to redirect_to(animal)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns the animal as @animal" do
+        animal = Animal.create! valid_attributes
+        put :update, {:id => animal.to_param, :animal => invalid_attributes}, valid_session
+        expect(assigns(:animal)).to eq(animal)
+      end
+
+      it "re-renders the 'edit' template" do
+        animal = Animal.create! valid_attributes
+        put :update, {:id => animal.to_param, :animal => invalid_attributes}, valid_session
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested animal" do
+      animal = Animal.create! valid_attributes
+      expect {
+        delete :destroy, {:id => animal.to_param}, valid_session
+      }.to change(Animal, :count).by(-1)
+    end
+
+    it "redirects to the animals list" do
+      animal = Animal.create! valid_attributes
+      delete :destroy, {:id => animal.to_param}, valid_session
+      expect(response).to redirect_to(animals_url)
+    end
+  end
+
 end
